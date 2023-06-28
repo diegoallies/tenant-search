9d657fc33c4b411568ce4fcc192288fb15dd442a
Diego Allies <diego.allies@arealytics.com.au>
2023-06-28 14:02:29 +0200

    save changes

---
+++ app/Tenant/[id]/page.tsx
@@ -0,0 +1,11 @@
+export default function aboutparams ({ params } : any) {
+
+    return (
+        <div>
+            <h1>
+                test page params : { params.id }
+            </h1>
+        </div>
+    )
+    
+    }
---
+++ app/Tenant/page.tsx
@@ -0,0 +1,11 @@
+export default function about () {
+
+    return (
+        <div>
+            <h1>
+                test page
+            </h1>
+        </div>
+    )
+    
+    }
