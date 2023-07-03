1ca8f392983782291709bcf30d6a6ad32054b4bb
Diego Allies <diego.allies@arealytics.com.au>
2023-07-03 12:21:05 +0200

    new

--- app/navbar.js
+++ app/navbar.js
@@ -1,24 +1,24 @@
-import Link from 'next/link';
-import './navstyles.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-
+import Link from "next/link";
+import "./navstyles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = ({ username }) => {
   return (
     <nav className="navbar">
       <div className="navbar__left">
-        <h1 className="navbar__heading">Analytics</h1>
+        <h1>
+          <Link legacyBehavior href={`/Tenant/`}>
+            <a className="navbar__heading">Arealytics</a>
+          </Link>
+        </h1>
       </div>
       <div className="navbar__right">
         <span className="navbar__username">{username}</span>
         <FontAwesomeIcon icon={faUser} className="navbar__profile-icon" />
-
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-
-
